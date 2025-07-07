2694075bcdcb602020bd492a6480c5a873d9fbc6
z5590213 <z5590213@vx13.orchestra.cse.unsw.EDU.AU>
2025-07-07 15:31:20 +1000

    added the function call for run target

---
+++ executor.h
@@ -0,0 +1,10 @@
+// this file holds the function call to run_target function
+#ifndef EXECUTOR_H
+#define EXECUTOR_H
+
+#define NORMAL 0
+#define CRASH 1
+
+int run_target(const char *target, const char *input_path);
+
+#endif
